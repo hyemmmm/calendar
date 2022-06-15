832d14b4e6e0ccd7c9bda905b6b524533ca204dc
hyemmmm <105183531+hyemmmm@users.noreply.github.com>
2022-06-15 18:21:29 +0900

    달력만들기 day1

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,22 +1,19 @@
+import CalendarTemplate from "./components/CalendarTemplate";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CalendarTemplate></CalendarTemplate>
     </div>
   );
 }
 
 export default App;
+
+// 달력 명세
+// 1. 시작 날짜(오늘) 생성 => new Date()
+// 연도, 월, 일, 요일 => 시작 요일 파악
+// 2. 시작날짜부터 마지막날까지 출력
+// 3. 날짜 변경 => 월 변경. 1번부터 다시
+// 4. 공휴일 처리
+// 5. 오늘 날짜 표시
